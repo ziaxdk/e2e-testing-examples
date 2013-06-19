2f34b0b158583b175fe0163086fa7e529508925e
ziaxdk <ziaxdk@gmail.com>
2013-06-19 15:20:50 +0200

    angular

---
+++ angular-scenario/karma.conf.js
@@ -0,0 +1,42 @@
+
+module.exports = function(karma) {
+  karma.configure({
+    //frameworks: ['ng-scenario'],
+    frameworks: ['ng-scenario'],
+
+    files: [
+      'spec/*.test.js'
+    ],
+
+    urlRoot: '/__karma/',
+
+    autoWatch: true,
+
+    proxies: {
+      '/': 'http://localhost:8080/'
+    },
+
+    browsers: [],
+
+    reporters: ['junit', 'progress'],
+
+    junitReporter: {
+      // will be resolved to basePath (in the same way as files/exclude patterns)
+      outputFile: 'test/test-results.xml'
+    },
+
+    //transports: ['websocket', 'flashsocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'],
+    transports: ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling'],
+
+    singleRun: false,
+
+    plugins: [
+      'karma-ng-scenario',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      //'karma-ie-launcher',
+
+      'karma-junit-reporter'
+    ]
+  });
+};
